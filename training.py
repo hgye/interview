a876dc32436600972881d8403a64c099a4484dcc
hgye <greenar@gmail.com>
2015-08-25 11:55:03 +0000

    first try, accuracy:0.25

--- training.py
+++ training.py
@@ -9,10 +9,10 @@ import re
 import operator
 import json
 import nltk
-import pdb
+# import pdb
 
 
-class dataSet(object):
+class trainDataSet(object):
 
     def __init__(self, folder):
         # training_folder = ('training-data')
@@ -34,25 +34,37 @@ class dataSet(object):
         self.class_list = []
 
         # training set
-        self.wordSet = defaultdict(list)
-        self.wordFreqdata = []
+        self.trainSet = defaultdict(list)
+        self.word_features = []
+        self.trainData = []
 
     def processData(self):
         # corpus is raw from json file
-        corpus = self.openJsonFile()
+        data = self.openJsonFile()
 
-        issue_li = corpus[0]["issues"]
+        issue_li = data[0]["issues"]
         field_li = [i["fields"] for i in issue_li]
 
+        corpus = []
         for d in field_li:
             proj = d["project"]["key"]
-            desc = d["description"]
-
+            desc = d['description']
             if desc:
-                self.appendWordSet(proj, desc)
+                self.addToCorpusAndGenDocument(proj, desc, corpus)
 
-        pdb.set_trace()
-        self.genWordFreqData()
+        # get all word, now it's time to get all word
+        all_words = nltk.FreqDist(w for w in corpus)
+        self.word_features = all_words.keys()[:2000]
+
+        # featureList = []
+
+        # for (proj, desc) in self.trainSet:
+        #    feature = self.document_features(desc, word_features)
+        #    featureList.append(feature)
+
+        # then we decide document feature
+        self.trainData = [(self.document_features(d), c)
+                          for (c, d) in self.trainSet.items()]
 
     def openJsonFile(self):
         data = []
@@ -65,7 +77,7 @@ class dataSet(object):
 
         return data
 
-    def appendWordSet(self, proj, desc):
+    def addToCorpusAndGenDocument(self, proj, desc, corpus):
         tokens = nltk.word_tokenize(desc)
         text = nltk.Text(tokens)
 
@@ -75,6 +87,8 @@ class dataSet(object):
         raw_words = [w for w in text if nonPunct.match(w)
                      if not hexNum.match(w)]
 
+        corpus += raw_words
+
         # find proj in class_list
         if proj in self.class_list:
             pass
@@ -82,33 +96,83 @@ class dataSet(object):
             self.class_list.append(proj)
 
         for p in self.class_list:
-            self.wordSet[p] += raw_words
+            self.trainSet[p] += raw_words
 
-    def genWordFreqData(self):
-        for t in self.wordSet.items():
-            raw_word_count = Counter(t[1])
+    def document_features(self, document):
+        document_words = set(document)
+        features = {}
+        for word in self.word_features:
+            features['contains(%s)' % word] = (word in document_words)
+        return features
+    # def appendWordSet(self, proj, desc):
+    #     tokens = nltk.word_tokenize(desc)
+    #     text = nltk.Text(tokens)
 
-            # result is list, pairs within [(word, freq)]
-            results = sorted(
-                raw_word_count.items(),
-                key=operator.itemgetter(1),
-                reverse=True)
+    #     # remove punctuation, count raw words
+    #     nonPunct = re.compile('.*[A-Za-z].*')
+    #     hexNum = re.compile('0x.*')
+    #     raw_words = [w for w in text if nonPunct.match(w)
+    #                  if not hexNum.match(w)]
 
-            self.wordFreqdata.append((t[0], results))
+    #     # find proj in class_list
+    #     if proj in self.class_list:
+    #         pass
+    #     else:
+    #         self.class_list.append(proj)
 
-    def getWordFreqData(self):
-        return self.wordFreqdata
+    #     for p in self.class_list:
+    #         self.trainSet[p] += raw_words
 
-training = dataSet('./training-data')
-training.processData()
-train_data = training.getWordFreqData()
+    # def genWordFreqData(self):
+    #     for t in self.trainSet.items():
+    #         raw_word_count = Counter(t[1])
 
-f = open('word.txt', 'w+')
-f.truncate()
-for i in train_data:
-    f.write(i[0])
-    f.write("\n")
-    for item in i[1][:100]:
-        f.write(u"{}\t".format(item))
-    f.write("\n")
-f.close()
+    #         # result is list, pairs within [(word, freq)]
+    #         results = sorted(
+    #             raw_word_count.items(),
+    #             key=operator.itemgetter(1),
+    #             reverse=True)
+
+    #         self.wordFreqdata.append((t[0], results))
+
+    def getTrainData(self):
+        return self.trainData
+
+# training = trainDataSet('./training-data')
+# training.processData()
+# trainSample = training.getTrainData()
+
+# for i in trainSample:
+# pdb.set_trace()
+
+class TextClassification(object):
+
+    def __int__(self):
+        self.trainData = []
+        self.testData = []
+
+    def fillSmaple(self, trainFolder='./training-data',
+                   testFolder='./test-data'):
+
+        training = trainDataSet(trainFolder)
+        training.processData()
+        self.trainData = training.getTrainData()
+
+        testing = trainDataSet(testFolder)
+        testing.processData()
+        self.testData = testing.getTrainData()
+
+    def classification(self):
+
+        print "train number:", len(self.trainData)
+        print "\n test number:", len(self.testData)
+
+        ## use NaiveBayesClassifie
+        classifier = nltk.NaiveBayesClassifier.train(self.trainData)
+        test_error = nltk.classify.accuracy(classifier, self.testData)
+        print "test accuracy:", test_error
+        return test_error
+
+classifier = TextClassification()
+classifier.fillSmaple()
+classifier.classification()
